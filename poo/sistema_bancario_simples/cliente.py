2fd13c6ed64274478cf536ad80c215130ed61391
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2022-04-18 19:38:00 -0300

    Adicionando continuação do desenvolvimento do exercicio.

--- poo/sistema_bancario_simples/cliente.py
+++ poo/sistema_bancario_simples/cliente.py
@@ -2,15 +2,17 @@ class Pessoa:
     def __init__(self, nome: str, idade: int):
         if not isinstance(nome, str):
             raise TypeError("Nome precisa ser do tipo String.")
+
         if not isinstance(idade, int):
             raise TypeError("Idade precisa ser do tipo Int.")
-        self.__nome = nome
-        self.__idade = idade 
+            
+        self._nome = nome
+        self._idade = idade 
     
 
     @property
-    def nome(self):
-        return self.__nome
+    def nome(self) -> str:
+        return self._nome
 
 
     @nome.setter
@@ -18,12 +20,12 @@ class Pessoa:
         if not isinstance(novo_nome, str):
             raise TypeError("Nome precisa ser do tipo String.")
 
-        self.__nome = novo_nome
+        self._nome = novo_nome
     
 
     @property
-    def idade(self):
-        return self.__idade
+    def idade(self) -> int:
+        return self._idade
 
 
     @idade.setter
@@ -31,14 +33,17 @@ class Pessoa:
         if not isinstance(nova_idade, int):
             raise TypeError("Idade precisa ser do tipo Int.")
             
-        self.__idade = nova_idade
+        self._idade = nova_idade
 
 
 class Cliente(Pessoa):
     def __init__(self, nome: str, idade: int):
         super().__init__(nome, idade)
-        self.__conta
+        self.conta = None
     
+
+    def inserir_conta(self, conta):
+        self.conta = conta
 
     
         
--- poo/sistema_bancario_simples/conta.py
+++ poo/sistema_bancario_simples/conta.py
@@ -1,50 +1,60 @@
 from abc import ABC, abstractmethod
-from typing import Union
+
 
 class Conta(ABC):
-    def __init__(self, agencia: int, conta: int, saldo: Union(float, int)):
-        self.__agencia = agencia
-        self.__conta = conta 
-        self.__saldo = saldo
+    def __init__(self, agencia, conta, saldo):
+        self.agencia = agencia
+        self.conta = conta 
+        self.saldo = saldo
     
-    
-    @property
-    def saldo(self):
-        return self.__saldo
 
-
-    def depositar(self, valor: Union(float, int)):
+    def depositar(self, valor):
         if valor < 0:
             raise ValueError("O valor a ser depositado precisa ser positivo.")
 
         if not isinstance(valor, (float, int)):
             raise TypeError("O valor precisa ser do tipo Int ou Float.")
 
-        self.__saldo += valor
+        self.saldo += valor
+        self.detalhes()
+
+
+    def detalhes(self):
+        print(f"Agência: {self.agencia}\nConta: {self.conta}\nSaldo: {self.saldo}")
 
 
     @abstractmethod
-    def sacar(self, valor):
-        pass
+    def sacar(self, valor): pass
 
 
 class ContaPoupanca(Conta):
-    def sacar(self, valor: Union(float, int)):
+    def sacar(self, valor):
         if not isinstance(valor, (float, int)):
             raise TypeError("O valor precisa ser do tipo Int ou Float.")
 
-        if (self.__saldo - valor) < 0:
+        if (self.saldo - valor) < 0:
             print("Saldo insuficiente.")
             return
-        self.__saldo -= valor
+
+        self.saldo -= valor
         print("Valor sacado som sucesso.")
+        self.detalhes()
 
 
 class ContaCorrente(Conta):
-    def __init__(self, agencia: int, conta: int, saldo: Union(float, int), limite: Union(float, int)):
+    def __init__(self, agencia: int, conta: int, saldo, limite=100):
         super().__init__(agencia, conta, saldo)
-        self.__limite = limite 
+        self.limite = limite 
     
     
-    def sacar(self, valor: Union(float, int)):
-        pass
+    def sacar(self, valor):
+        if not isinstance(valor, (float, int)):
+            raise TypeError("O valor precisa ser do tipo Int ou Float.")
+
+        if (self.saldo + self.limite) < valor:
+            print("Saldo insuficiente.")
+            return
+
+        self.saldo -= valor
+        print("Valor sacado som sucesso.")
+        self.detalhes()
