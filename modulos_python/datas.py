8692def715f2507c620811d49abe51883c8f188f
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2022-04-19 15:33:06 -0300

    Adicionando conteudo sobre datas em portugues e meses do ano.

--- modulos_python/datas.py
+++ modulos_python/datas.py
@@ -1,6 +1,9 @@
 # https://docs.python.org/3.0/library/datetime.html
 
 from datetime import datetime, timedelta
+from locale import setlocale, LC_ALL
+from calendar import mdays, monthrange
+
 
 data_0 = datetime(2022, 10, 22, 12, 50, 23) # criar datas: Ano, mes, dia, hora, minuto e segundo.
 print()
@@ -18,6 +21,7 @@ print(f'data_2: {data_2}')
 print()
 
 print("#"*50)
+print()
 
 data_3 = datetime.strptime('25/01/2000 20:00:10', "%d/%m/%Y %H:%M:%S")
 print(f'data_3: {data_3.strftime("%d/%m/%Y %H:%M:%S")}')
@@ -32,10 +36,38 @@ print(f'Diferença entre data_4 - data_3: {dif_data}')
 print(f'Diferença entre data_4 - data_3 em segundos: {dif_data.total_seconds()}')
 print(f'Diferença entre data_4 - data_3 em dias: {dif_data.days}')
 
+print()
 print("#"*50)
+print()
 
 data_5 = datetime(2022, 12, 19, 22, 23, 57)
 
 print(f'hora da data_5: {data_5.time()}') # mostrando apenas as horas de uma data.
 print(f'data da data_5: {data_5.date()}') # mostrando apenas a data de uma data.
 print(f'dia da semana da data_5: {data_5.weekday()}') # mostrando apenas o dia da semana de uma data; 0 - segunda -> 6 - domingo.
+
+print()
+print("#"*50)
+print()
+
+data_6 = datetime.now()
+data_6_formatada = data_6.strftime('%A, %d de %B de %Y')
+print(f"data_6_formatada: {data_6_formatada}")
+
+setlocale(LC_ALL, 'pt_BR.utf-8') # editando o local e com isso mudando a lingua para mostrar as datas em portugues.
+
+data_6_formatada = data_6.strftime('%A, %d de %B de %Y')
+print(f"data_6_formatada (Mudando a região): {data_6_formatada}")
+
+print()
+print("#"*50)
+print()
+
+data_7 = datetime.now()
+mes_atual = int(data_7.strftime('%m'))
+print(mdays) # ultimo ou quantidade de dias que tem os meses no ano (OBS: Não funciona em ano bissexto).
+print(f'quantos dias tem o mês atual: {mdays[mes_atual]}') # ultimo dia ou quantidade de dias que tem o mes atual.
+
+qnt_dias_dos_meses_no_ano_atual = [monthrange(datetime.now().year, mes)[1] for mes in range(1, 13)] # para funciona em ano bissexto
+
+print(qnt_dias_dos_meses_no_ano_atual)
