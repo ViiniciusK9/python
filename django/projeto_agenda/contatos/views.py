81d3925d7e138f76df1664103e2167396679d4c6
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2022-04-28 18:11:58 -0300

    Adicionando tratamento de mensagens para o usuario, mensagens de error, warning, debug, success e info.

--- django/projeto_agenda/contatos/views.py
+++ django/projeto_agenda/contatos/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models.functions import Concat
 from django.core.paginator import Paginator
 from django.db.models import Q, Value
+from django.contrib import messages
 from django.http import Http404
 from .models import Contato
 
@@ -30,8 +31,9 @@ def ver_contato(request, contato_id):
 def busca(request):
     termo = request.GET.get('termo')
 
-    if termo is None:
-        raise Http404()
+    if termo is None or not termo:
+        messages.add_message(request, messages.ERROR, 'Campo termo não pode ficar vazio.')
+        return redirect('index')
 
     campos = Concat('nome', Value(' '), 'sobrenome')
 
@@ -44,6 +46,7 @@ def busca(request):
     paginator = Paginator(contatos, 20)
     page = request.GET.get('p')
     contatos = paginator.get_page(page)
+    messages.add_message(request, messages.SUCCESS, 'Busca realizada com sucesso.')
     return render(request, 'contatos/busca.html', {
         'contatos': contatos
     })
