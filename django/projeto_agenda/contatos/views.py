dfad5e4c1c5b52e80ca9baa1a6112442e6bce1ff
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2022-04-28 09:27:54 -0300

    Corrigindo levantamento de erro correto de 500 para 404 na busca de ids que não existem.

--- django/projeto_agenda/contatos/views.py
+++ django/projeto_agenda/contatos/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Contato
 
 
@@ -8,9 +9,19 @@ def index(request):
         'contatos': contatos
     })
 
-
 def ver_contato(request, contato_id):
+    contato = get_object_or_404(Contato, id=contato_id)
+    return render(request, 'contatos/ver_contato.html', {
+        'contato': contato
+    })
+
+
+"""
+try:
     contato = Contato.objects.get(id=contato_id)
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
     })
+except Contato.DoesNotExist as error:
+    raise Http404()
+"""
