d5afde067e6c485789d0331b9fe07ae3d34a7d96
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2022-04-28 09:59:56 -0300

    Adicionando paginação na vizualização da agenda.

--- django/projeto_agenda/contatos/views.py
+++ django/projeto_agenda/contatos/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from django.http import Http404
 from .models import Contato
 
 
 def index(request):
     contatos = Contato.objects.all()
+    paginator = Paginator(contatos, 20)
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
     return render(request, 'contatos/index.html', {
         'contatos': contatos
     })
+
 
 def ver_contato(request, contato_id):
     contato = get_object_or_404(Contato, id=contato_id)
