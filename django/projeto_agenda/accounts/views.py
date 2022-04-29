1033c64ce23235712804d7cd9217cf25d77292db
Vinicius Koncicoski <viniciuskoncicoski@gmail.com>
2022-04-29 17:03:59 -0300

    Adicionando formulario para fazer cadastro de um novo contato na agenda pelo dashboard de um usuario logado.

--- django/projeto_agenda/accounts/views.py
+++ django/projeto_agenda/accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
+from .models import FormContato
 
 
 def login(request):
@@ -85,4 +86,17 @@ def cadastro(request):
 
 @login_required(redirect_field_name='login')
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    if request.method != 'POST':
+        form = FormContato()
+        return render(request, 'accounts/dashboard.html', {'form': form})
+
+    form = FormContato(request.POST, request.FILES)
+
+    if not form.is_valid():
+        messages.error(request, 'Erro ao enviar formulário.')
+        form = FormContato(request.POST)
+        return render(request, 'accounts/dashboard.html', {'form': form})
+
+    form.save()
+    messages.success(request, f'Contato {request.POST.get("nome")} salvo com sucesso!')
+    return redirect('dashboard')
